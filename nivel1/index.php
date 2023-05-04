f757f393d48b6da2afdcafba77a76f55d2cecab8
brycoti <bryancozartitos@gmail.com>
2023-05-04 10:01:42 +0200

    nivel 1 listo para entregar

--- nivel1/index.php
+++ nivel1/index.php
@@ -1,29 +1,30 @@
 <?php
-// Ejercicio 1: 
+// Ejercicio 1:
+echo "<h2>Ejercicio 1</h2>"; 
 $listaNumeros  = array(44, 7, 89, 3333, 7653);
 
 print_r($listaNumeros);
 echo "<br><br><hr>";
-?>
 
-<?php
 // ejercicio 2:
+echo "<h2>Ejercicio 2</h2>";
 $X = array(10, 20, 30, 40, 50, 60);
 
 echo 'La longitud de $X es '.  var_dump($X) . "de " . count($X) . "." . "<br><br>";
+echo " Eliminando la posición [4] <br><br>";
 
-array_slice($X,1,1);
+unset($X[4]);
+print_r($X);  
+
+echo " <br><br> Normalizando la array <br><br>";
+
+$X = array_values($X);
+
 print_r($X);
-echo "<br><br>";
-array_values($X);
-print_r($X);
-        // faltar mirar el eliminar un elemento y encontrar funcion que normalice
 echo "<br><br><hr>";
-?>
 
-<?php
 // ejercicio 3 
-
+echo "<h2>Ejercicio 3</h2>";
 function buscarCaracter($arrayPalabras, $caracter) {
     foreach ($arrayPalabras as $palabra) {
         if (strpos($palabra, $caracter) !== false) {
@@ -34,15 +35,20 @@ function buscarCaracter($arrayPalabras, $caracter) {
 }
 
 $saludos = array('hola', 'buenos dias', 'ciao', 'adios', 'hello', 'hasta luego');
-print_r($saludos);
-$hayCaracter = buscarCaracter($saludos, "s");
-echo "<br>";
-var_dump($hayCaracter);
-echo  "<br><br><hr>";
-?>
 
-<?php
+print_r($saludos);
+
+$hayCaracter = buscarCaracter($saludos, "s");
+
+echo " Buscando si hay el carácter 's' como parametro: <br>";
+
+var_dump($hayCaracter);
+
+echo  "<br><br><hr>";
+
 // ejercicio 4 
+echo "<h2>Ejercicio 4</h2>";
+
 $presentacion = array(
   "nombre" => "Bry",
   "edad" =>  100,
@@ -51,6 +57,4 @@ $presentacion = array(
 );
 
 print_r($presentacion);
-
-
 ?>
